143082504ab9530b1563a61646df04b92deab8a4
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-06-05 19:38:15 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
-
 import { Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../components/layout/Layout';
+import BackgroundImage from 'gatsby-background-image';
+
+import { graphql, useStaticQuery } from 'gatsby';
+
+const query = graphql`
+	query {
+		allContentfulHomePageBackground {
+			nodes {
+				backgroundImage {
+					fluid(quality: 90, maxWidth: 4000) {
+						...GatsbyContentfulFluid
+					}
+				}
+			}
+		}
+	}
+`;
 
 export default function Home() {
+	const data = useStaticQuery(query);
+	const backgroundImage = data.allContentfulHomePageBackground.nodes[0].backgroundImage.fluid;
 	return (
 		<Layout>
-			<Col>hello</Col>
+			<BackgroundImage
+				fluid={backgroundImage}
+				style={{ height: '102vh', width: '100%', marginTop: '-3rem', zIndex: '-1' }}
+			/>
 		</Layout>
 	);
 }
