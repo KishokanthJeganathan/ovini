b5890aafdd6b9e40417201c49c2df060cf27ac80
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-06-05 18:58:29 +0200

    plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,22 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+	/* Your site config here */
+	plugins: [
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: process.env.CONTENTFUL_SOURCE_ID,
+				// Learn about environment variables: https://gatsby.dev/env-vars
+				accessToken: process.env.CONTENTFUL_ACESS_TOKEN
+			}
+		}
+	]
+};
