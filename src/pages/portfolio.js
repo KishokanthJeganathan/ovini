192968dd7abdce8e2c923b9aacfb35fc91c03e12
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-06-06 22:11:48 +0200

    Create portfolio.js

---
+++ src/pages/portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/layout/Layout';
+import { Col } from 'react-bootstrap';
+
+export default function portfolio() {
+	return (
+		<Layout>
+			<Col>hello</Col>
+		</Layout>
+	);
+}
