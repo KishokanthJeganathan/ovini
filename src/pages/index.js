0d01443aa76953361200ab3a3bf0399d3b7fcfef
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-06-11 00:03:06 +0200

    dd

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../components/layout/Layout';
 import BackgroundImage from 'gatsby-background-image';
 import styles from '../pages/index.module.css';
-
+import { Col, Row } from 'react-bootstrap';
 import { graphql, useStaticQuery } from 'gatsby';
 
 const query = graphql`
@@ -22,25 +22,16 @@ const query = graphql`
 
 export default function Home() {
 	const data = useStaticQuery(query);
-	const backgroundImage = data.allContentfulHomePageBackground.nodes[0].backgroundImage.fluid;
+
 	return (
 		<Layout>
-			{/* using inline styles here due to the nature of BackgroundImage */}
-			<BackgroundImage
-				fluid={backgroundImage}
-				style={{
-					height: '100vh',
-					width: '100%',
-					marginTop: '-3rem',
-					zIndex: '-1',
-					display: 'flex',
-					alignItems: 'center'
-				}}
-			>
-				<h1 className={styles.brand}>
-					<span className={styles.ovini}>OVINI</span> <br /> HAUTE COUTURE
-				</h1>
-			</BackgroundImage>
+			<Col>
+				<Row className={styles.magazineHolder}>
+					<Col xs={11} sm={5} className={styles.magazine}>
+						Hello
+					</Col>
+				</Row>
+			</Col>
 		</Layout>
 	);
 }
