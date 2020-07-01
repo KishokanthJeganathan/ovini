fadb16b59d43b27f485ed7db32055f3f338a77e6
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-07-01 16:08:54 +0200

    Update Seo.js

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -31,7 +31,7 @@ const SEO = ({ title, description, image, article }) => {
 
 			{seo.url && <meta property="og:url" content={seo.url} />}
 
-			{article ? <meta property="og:type" content="article" /> : <meta property="og:type" content="website" />}
+			{<meta property="og:type" content="website" />}
 
 			{seo.title && <meta property="og:title" content={seo.title} />}
 
